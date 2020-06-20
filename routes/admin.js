cee465f50cf2cb539f798a3bebccdfb6f4961517
nakeusa12 <snakeusa8@gmail.com>
2020-06-20 19:51:07 +0700

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
+const { upload, uploadMultiple } = require("../middlewares/multer");
+
+router.get("/dashboard", adminController.viewDashboard);
+// -----
+
+router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+
+// -----
+
+router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
+// -----
+
+router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+
+// -----
+
+router.get("/booking", adminController.viewBooking);
+
+module.exports = router;
