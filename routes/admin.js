e46e3b10cf13119740698bbfceaf1857fcb89166
nakeusa12 <snakeusa8@gmail.com>
2020-06-21 10:52:32 +0700

    buat page feature dan activity

--- routes/admin.js
+++ routes/admin.js
@@ -26,6 +26,8 @@ router.get("/item/:id", adminController.showEditItem);
 router.put("/item/:id", uploadMultiple, adminController.editItem);
 router.delete("/item/:id/delete", adminController.deleteItem);
 
+router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
+
 // -----
 
 router.get("/booking", adminController.viewBooking);
