802c61df93e11e8d5e016d126e4ee6b2478bb95f
nakeusa12 <snakeusa8@gmail.com>
2020-06-22 20:36:41 +0700

    CRUD Activity

--- models/Booking.js
+++ models/Booking.js
@@ -4,37 +4,65 @@ const { ObjectId } = mongoose.Schema;
 const bookingSchema = new mongoose.Schema({
   bookingStartDate: {
     type: Date,
-    required: true,
+    required: true
   },
   bookingEndDate: {
     type: Date,
-    required: true,
+    required: true
   },
-  itemId: [
-    {
-      _id: { type: ObjectId, ref: "Item", required: true },
-      price: { type: Number, required: true },
-      night: { type: Number, required: true },
+  invoice: {
+    type: String,
+    required: true
+  },
+  itemId: {
+    _id: {
+      type: ObjectId,
+      ref: 'Item',
+      required: true
     },
-  ],
-  memberId: [{ type: ObjectId, ref: "Member" }],
-  bankId: [{ type: ObjectId, ref: "Bank" }],
-  proofPayment: {
-    type: String,
-    required: true,
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: true
+    }
   },
-  bankFrom: {
-    type: String,
-    required: true,
+  total: {
+    type: Number,
+    required: true
   },
-  accountHolder: {
-    type: String,
-    required: true,
+  memberId: {
+    type: ObjectId,
+    ref: 'Member'
   },
-  status: {
-    type: String,
-    required: true,
+  bankId: {
+    type: ObjectId,
+    ref: 'Bank'
   },
-});
+  payments: {
+    proofPayment: {
+      type: String,
+      required: true
+    },
+    bankFrom: {
+      type: String,
+      required: true
+    },
+    accountHolder: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      default: 'Proses'
+    }
+  }
+})
 
-module.exports = mongoose.model("Booking", bookingSchema);
+module.exports = mongoose.model('Booking', bookingSchema)
