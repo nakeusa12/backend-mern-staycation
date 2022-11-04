92757dfce72554a805e7465767359bcc9840671c
nakeusa12 <snakeusa8@gmail.com>
2022-11-04 15:47:23 +0700

    sessions

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 600000 },
+    cookie: { maxAge: 3600000 },
   })
 );
 app.use(flash());
