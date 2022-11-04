ed1b7032f3b76ef823df85ad445c3af5411d57f0
nakeusa12 <snakeusa8@gmail.com>
2022-11-04 15:09:55 +0700

    add

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 },
+    cookie: { maxAge: 600000 },
   })
 );
 app.use(flash());
